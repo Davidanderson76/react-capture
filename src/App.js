d7c01c9f69dea7f994e29d668bb3038f2c338094
David Anderson <david.anderson1993@gmail.com>
2021-07-01 00:58:58 -0500

    added styled components to about section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+//GLOBAL STYLE
+import GlobalStyle from "./components/GlobalStyle";
 //IMPORT PAGES
 import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,22 +1,24 @@
 import React from "react";
 import me from "../img/me1.PNG";
+//STYLED
+import styled from "styled-components";
 
 const AboutSection = () => {
   return (
-    <div>
-      <div className="description">
+    <About>
+      <Description>
         <div className="title">
-          <div className="hide">
+          <Hide>
             <h2> I work to make</h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>
               Your <span> dreams </span> come
             </h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>true.</h2>
-          </div>
+          </Hide>
         </div>
         <p>
           Full stack software engineer with over a decade of management
@@ -29,12 +31,44 @@ const AboutSection = () => {
           from ideation to execution.
         </p>
         <button>Contact Me</button>
-      </div>
-      <div className="image">
+      </Description>
+      <Image>
         <img src={me} alt="David Anderson" />
-      </div>
-    </div>
+      </Image>
+    </About>
   );
 };
+
+//styled components
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
 
 export default AboutSection;
